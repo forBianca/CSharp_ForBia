32229b97b2ef9af2819498b76716d5bd5265990f
Lucas-M3dina <Lucas.m3dina@gmail.com>
2021-08-21 02:18:21 -0300

    Pra ti

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ForEla___PT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool logado = false;
+            string senha;
+
+            Console.WriteLine("\n Oieee amorr <3 \n");
+
+            Console.WriteLine($@"
+User: Morango
+Senha: dGVBbW9NdWl0b0JpYWFh
+            
+             ");
+
+            do
+            {
+                Console.Write("Digite a sua senha: ");
+                senha = Console.ReadLine().ToLower();
+                if (senha == "teamomuitobiaaa")
+                {
+                    logado = true;
+                }
+                else if (senha == "dgvbbw9ndwl0b0jpywfh")
+                {
+                    Console.WriteLine("\n Precisa decodificar meu amor, ja tentou base64 ? \n");
+                    logado = false;
+                }
+                else if (senha == "luiz")
+                {
+                    Console.WriteLine("\n Curiosoooooooooo \n");
+                    logado = false;
+                }
+                else
+                {
+                    logado = false;
+                    Console.WriteLine("Senha Incorreta\n");
+                }
+                
+            } while (logado == false);
+
+
+            Console.WriteLine("Amor");
+            Console.ReadLine();
+
+            Console.WriteLine("Primeiro queria dizer que essa pessoinha que está do seu lado agora te ama muito e td que ele quer eh te ver sorrir <3 ");
+            Console.ReadLine();
+
+            Console.WriteLine("Sei que como vc disse eu estava indo umm pouquinho rapido, dscp ta? é só medo de perder a oportunidade de ficar ao lado da mina mais incrivel que ja conheci, você");
+            Console.ReadLine();
+
+            Console.WriteLine("A meuu bem são 01:37 da manhã de sexta e eu só consigo pensar em você, você faz muita falta, quando não te vejo parece que esta faltando algo no meu dia ");
+            Console.ReadLine();
+
+            Console.WriteLine("Te amo de verdade, estar com você é muito mais que só estar com uma mina gata, estar com vc eh...");
+            Console.ReadLine();
+
+            Console.WriteLine("Curtir seu dom de me fazer sorrir");
+            Console.ReadLine();
+
+            Console.WriteLine("Admirar seu modo de pensar e de expressar");
+            Console.ReadLine();
+
+            Console.WriteLine("Perceber que tem alguem mais desastrado que eu kkkkk");
+            Console.ReadLine();
+
+            Console.WriteLine("Mas o mais importante é que quando olho pro lado vejo uma mulher incrivel em todos os sentidos");
+            Console.ReadLine();
+
+            Console.WriteLine("E posso enxergar uma pessoa na qual posso confiar ");
+            Console.ReadLine();
+
+            Console.ReadLine();
+
+            Console.WriteLine("<3 Te amo muito meu amorrrrrrrrrrrrrr <3 ");
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
